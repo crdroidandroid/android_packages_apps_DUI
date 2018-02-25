7abb6c338b36740d2bc7dadcb5ccb3e15edd5b71
Pranav Vashi <neobuddy89@gmail.com>
2018-02-26 03:52:08 +0530

    Add delay while applying dark intensity
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/systemui/navigation/smartbar/SmartBarView.java
+++ src/com/android/systemui/navigation/smartbar/SmartBarView.java
@@ -89,6 +89,9 @@ public class SmartBarView extends BaseNavigationBar {
     final static int PULSE_FADE_OUT_DURATION = 250;
     final static int PULSE_FADE_IN_DURATION = 200;
 
+    private Handler mHandler;
+    private static boolean mIssued;
+
     static final int IME_HINT_MODE_HIDDEN = 0;
     static final int IME_HINT_MODE_ARROWS = 1;
     static final int IME_HINT_MODE_PICKER = 2;
@@ -198,6 +201,7 @@ public class SmartBarView extends BaseNavigationBar {
         super(context);
         mBarTransitions = new SmartBarTransitions(this);
         mEditor = new SmartBarEditor(this);
+        mHandler = new Handler();
         mSmartObserver.addListener(mObservable);
         createBaseViews();
 
@@ -459,7 +463,15 @@ public class SmartBarView extends BaseNavigationBar {
     }
 
     private void reapplyDarkIntensity() {
-        mBarTransitions.reapplyDarkIntensity();
+        if (mIssued) return;
+        mIssued = true;
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mIssued = false;
+                mBarTransitions.reapplyDarkIntensity();
+            }
+        }, 200);
     }
 
     @Override
